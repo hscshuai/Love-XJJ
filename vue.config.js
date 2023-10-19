2698dde788e31c001581bb955850053078637088
hansicong <544316483@qq.com>
2023-10-19 15:53:36 +0800

    新增打包时文件复制插件

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,16 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require('@vue/cli-service');
+const CopyPlugin = require('copy-webpack-plugin');
+
 module.exports = defineConfig({
   transpileDependencies: true,
   outputDir: 'docs', // 打包的目录
+  configureWebpack: {
+    plugins: [
+      new CopyPlugin({
+        patterns: [
+          { from: 'public/config/CNAME', to: '' },
+        ],
+      }),
+    ],
+  }
 })
